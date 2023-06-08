b60cb3b05290a11f4f728a5e8bd58070b142a6fd
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-06-08 23:17:55 +0600

    coffeecollection

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const coffeeCollection = client.db("coffeeDB");
+
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
